56834eab6b8af08b5bb38c7b5cc44858767eafff
Anthony Scinocco <scinocco.a@gmail.com>
2020-10-02 13:43:23 -0400

    implemented actived state for navbar links

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  currentUrl: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((navEnd: NavigationEnd) => {
+      if (navEnd.url) {
+        this.currentUrl = navEnd.url;
+      }
+    });
+   }
 
   ngOnInit() {
+    console.log('this.currentUrl', this.currentUrl);
   }
 
+  ngOnChanges() {
+    console.log('this.currentUrl', this.currentUrl);
+  }
 }
